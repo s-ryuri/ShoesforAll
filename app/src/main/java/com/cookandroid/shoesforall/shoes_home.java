278b6a46b39415aa877ce4c89815e281dcf83d05
s-ryuri <fbfbf1@naver.com>
2021-10-16 14:25:30 +0900

    상세 페이지 구현

---
+++ app/src/main/java/com/cookandroid/shoesforall/shoes_home.java
@@ -0,0 +1,13 @@
+package com.cookandroid.shoesforall;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class shoes_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoes_home);
+    }
+}

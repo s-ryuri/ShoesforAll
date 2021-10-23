d4b75719df773a433a5efebddcff25a4c0e9ecc4
s-ryuri <fbfbf1@naver.com>
2021-10-23 23:00:41 +0900

    홈화면 배경색깔 흰색으로 변경 및 이미지 누르면 상세페이지로

--- app/src/main/java/com/cookandroid/shoesforall/home_fragment.java
+++ app/src/main/java/com/cookandroid/shoesforall/home_fragment.java
@@ -1,9 +1,11 @@
 package com.cookandroid.shoesforall;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +13,23 @@ import androidx.fragment.app.Fragment;
 
 public class home_fragment extends Fragment {
     private View view;
+    private ImageButton ultra_image;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.home,container,false);
+
+        ultra_image = (ImageButton) view.findViewById(R.id.ultra_image); //fragment에서 findViewByid는 view.을 이용해서 사용
+
+        ultra_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),detail_page.class); //fragment라서 activity intent와는 다른 방식
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }

8355c349f30a3f18280f40d1f6eaab21f1fe64f7
s-ryuri <fbfbf1@naver.com>
2021-10-24 23:36:53 +0900

    홈 fragment의 버튼 for문 이용 초기화 및 상세 페이지에 임시 viewFilpper 구현

--- app/src/main/java/com/cookandroid/shoesforall/detail_page.java
+++ app/src/main/java/com/cookandroid/shoesforall/detail_page.java
@@ -2,12 +2,16 @@ package com.cookandroid.shoesforall;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class detail_page extends AppCompatActivity {
-
+    private ViewFlipper viewFlipper1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_page);
+
     }
 }

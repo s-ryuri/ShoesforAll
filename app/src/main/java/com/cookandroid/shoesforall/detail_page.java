07538fd4f39feb1bfd50069b635a6ac9315c7a0a
s-ryuri <fbfbf1@naver.com>
2021-11-06 00:53:46 +0900

    recylcerview에서 클릭하면 사진빼고 정보 다 넘어가게 구현

--- app/src/main/java/com/cookandroid/shoesforall/detail_page.java
+++ app/src/main/java/com/cookandroid/shoesforall/detail_page.java
@@ -7,18 +7,33 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class detail_page extends AppCompatActivity {
     private ViewFlipper viewFlipper1;
-    private Button btn1;
+    private ImageView detail_image;
+    private TextView shoesName_txt,price_txt,description_txt,information_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_page);
-        btn1 = (Button) findViewById(R.id.btn1);
+
         Intent intent = getIntent();
-        btn1.setText(intent.getStringExtra("TEXT"));
+        detail_image = (ImageView) findViewById(R.id.detail_image);
+        shoesName_txt = (TextView) findViewById(R.id.shoesName_txt);
+        price_txt = (TextView) findViewById(R.id.price_txt);
+        description_txt = (TextView) findViewById(R.id.description_txt);
+        information_txt = (TextView) findViewById(R.id.information_txt);
+
+
+        detail_image.setImageBitmap(intent.getParcelableExtra("IMAGE"));
+        shoesName_txt.setText(intent.getStringExtra("NAME"));
+        price_txt.setText(intent.getStringExtra("PRICE"));
+        description_txt.setText(intent.getStringExtra("DESCRIPTION"));
+        information_txt.setText(intent.getStringExtra("INFORMATION"));
+
 
     }
 }

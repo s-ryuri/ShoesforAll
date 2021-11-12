686dcf631a3c82bbbafb0eb4921e9c9d9aad9838
s-ryuri <fbfbf1@naver.com>
2021-11-12 23:57:15 +0900

    검색창 구현 해볼려고 했는데?

---
+++ app/src/main/java/com/cookandroid/shoesforall/searchActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.shoesforall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
